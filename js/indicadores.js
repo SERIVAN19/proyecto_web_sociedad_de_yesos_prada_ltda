6d33278d9abb89f130b8e511cadff9cb02bcaf0f
SERIVAN19 <sbuenoardila@gmail.com>
2026-02-26 11:32:10 -0500

    feat 10: agregando secciones

---
+++ js/indicadores.js
@@ -0,0 +1,18 @@
+const counters = document.querySelectorAll('.contador');
+
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+        const increment = target / 200;
+
+        if (count < target) {
+            counter.innerText = Math.ceil(count + increment);
+            setTimeout(updateCount, 10);
+        } else {
+            counter.innerText = target;
+        }
+    };
+
+    updateCount();
+});
